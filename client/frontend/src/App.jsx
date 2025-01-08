ce40c50fa4aca2469bb5490377e097e05cd2bd40
ArchieTansaria <archietans.work@gmail.com>
2025-01-08 19:24:29 +0530

    Initial commit

---
+++ client/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./components/Landing/Home.jsx";
+
+function App() {
+	// const [count, setCount] = useState(0)
+
+	return <Home />;
+}
+
+export default App;
