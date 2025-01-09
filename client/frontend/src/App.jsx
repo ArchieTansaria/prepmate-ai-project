d8d6cea4af899f4244af5f0dfa3b09a5b1216926
ArchieTansaria <archietans.work@gmail.com>
2025-01-09 17:25:04 +0530

    second

--- client/frontend/src/App.jsx
+++ client/frontend/src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Landing/Home.jsx";
+import Home from "./components/Landing/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Login from "./components/Login/Login";
+import SignUp from "./components/Signup/Signup";
 
 function App() {
 	// const [count, setCount] = useState(0)
 
-	return <Home />;
+	return (
+		<div>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/signup" element={<SignUp />} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</div>
+	);
 }
 
-export default App;
+export default function AppWrapper() {
+	return (
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	);
+}
